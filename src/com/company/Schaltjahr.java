f04c6f5277cdbda9bb717655dcf804773349d218
Chris <christopher@voglgsang.com>
2020-12-17 15:03:23 +0100

    renamed Main

--- src/com/company/Schaltjahr.java
+++ src/com/company/Schaltjahr.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class Schaltjahr {
 
     public static void main(String[] args) {
 	Scanner input = new Scanner(System.in);
