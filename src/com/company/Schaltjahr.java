143872725a09e4cc09fae776c908c69db1ece48a
Chris <christopher@voglgsang.com>
2020-12-17 15:09:20 +0100

    added comments

--- src/com/company/Schaltjahr.java
+++ src/com/company/Schaltjahr.java
@@ -5,13 +5,16 @@ import java.util.Scanner;
 public class Schaltjahr {
 
     public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	System.out.print("Bitte Jahr eingeben: ");
-	int eingabe = input.nextInt();
-	if(eingabe % 4 == 0 && (eingabe % 100 != 0 || eingabe % 400 == 0)) {
-		System.out.print(eingabe + " ist ein Schaltjahr!");
+	Scanner input = new Scanner(System.in);										//scanner implementation
+
+	System.out.print("Bitte Jahr eingeben: ");									//first output
+	int eingabe = input.nextInt();												//scanner
+
+	if(eingabe % 4 == 0 && (eingabe % 100 != 0 || eingabe % 400 == 0)) {		//needs and, or
+		System.out.print(eingabe + " ist ein Schaltjahr!");						//output if true
+
 	} else {
-		System.out.print(eingabe + " ist kein Schaltjahr!");
+		System.out.print(eingabe + " ist kein Schaltjahr!");					//output if false
 	}
     }
 }
